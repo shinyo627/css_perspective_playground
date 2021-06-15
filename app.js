dc7df4db753018598a4bb87f5d1255fdd7831546
shinyo627 <shinyo627@gmail.com>
2021-06-15 11:56:37 +0200

    implemented most of functions for the application except copy button in document

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
 const vm = Vue.createApp({
   data() {
     return {
-      range: 0,
+      perspective: 0,
+      x: 0,
+      y: 0,
+      z: 0,
     };
   },
   methods: {
-    setRange(e) {
-      e.preventDefault;
-
-      this.range += parseInt(e.target.value);
-      console.log(this.range);
+    setPerspective(e) {
+      this.perspective = parseInt(e.target.value);
+      console.log(this.perspective);
+    },
+    setX(e) {
+      this.x = parseInt(e.target.value);
+      console.log(this.x);
+    },
+    setY(e) {
+      this.y = parseInt(e.target.value);
+      console.log(this.y);
+    },
+    setZ(e) {
+      this.z = parseInt(e.target.value);
+      console.log(this.z);
+    },
+    reset(e) {
+      this.perspective = 0;
+      this.x = 0;
+      this.y = 0;
+      this.z = 0;
     },
   },
-  computed: {},
+  computed: {
+    box_styles() {
+      return [
+        {
+          transition: 'transform 2s',
+        },
+        {
+          transform: `perspective(${this.perspective}px) rotateX(${this.x}deg) rotateY(${this.y}deg) rotateZ(${this.z}deg)`,
+        },
+      ];
+    },
+    copy() {
+      const copied = [this.perspective, this.x, this.y, this.z];
+
+      return copied;
+    },
+  },
 });
 
 vm.mount('#app');
