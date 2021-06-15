bb2c280d2257a3373524cd38ded15adb8d52ce9a
shinyo627 <shinyo627@gmail.com>
2021-06-15 16:39:56 +0200

    Revised with new changes from tutorial

--- app.js
+++ app.js
@@ -1,51 +1,41 @@
 const vm = Vue.createApp({
   data() {
     return {
-      perspective: 0,
+      perspective: 100,
       x: 0,
       y: 0,
       z: 0,
     };
   },
   methods: {
-    setPerspective(e) {
-      this.perspective = parseInt(e.target.value);
-      console.log(this.perspective);
-    },
-    setX(e) {
-      this.x = parseInt(e.target.value);
-      console.log(this.x);
-    },
-    setY(e) {
-      this.y = parseInt(e.target.value);
-      console.log(this.y);
-    },
-    setZ(e) {
-      this.z = parseInt(e.target.value);
-      console.log(this.z);
-    },
-    reset(e) {
-      this.perspective = 0;
+    reset() {
+      this.perspective = 100;
       this.x = 0;
       this.y = 0;
       this.z = 0;
     },
+    copy() {
+      const el = document.createElement('textarea');
+
+      // Hiding textarea
+      el.setAttribute('readonly', '');
+      el.style.position = 'absolute';
+      el.style.left = '-9999px';
+
+      el.value = `transform: ${this.box.transform}`;
+
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+    },
   },
   computed: {
-    box_styles() {
-      return [
-        {
-          transition: 'transform 2s',
-        },
-        {
-          transform: `perspective(${this.perspective}px) rotateX(${this.x}deg) rotateY(${this.y}deg) rotateZ(${this.z}deg)`,
-        },
-      ];
-    },
-    copy() {
-      const copied = [this.perspective, this.x, this.y, this.z];
-
-      return copied;
+    box() {
+      return {
+        transition: 'transform 2s',
+        transform: `perspective(${this.perspective}px) rotateX(${this.x}deg) rotateY(${this.y}deg) rotateZ(${this.z}deg)`,
+      };
     },
   },
 });
